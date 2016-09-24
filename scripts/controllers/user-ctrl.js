101070ae94adb740e5ac95cfa9a6bbd9709c84e0
xiaomeng jin <tracy@parsehub.com>
2016-09-24 16:09:52 -0400

    added getStarted

--- scripts/controllers/user-ctrl.js
+++ scripts/controllers/user-ctrl.js
@@ -8,12 +8,25 @@ function userCtrl($scope, $http,$rootScope) {
 
     $http({
         method: 'GET',
-        url: 'https://api.myjson.com/bins/53yv0',
+        url: 'https://api.myjson.com/bins/4q238',
         headers: {'Content-Type': 'application/json'}
     }).success(function (result) {
+        $scope.id = result.data[0].id;
+        $scope.name = result.data[0].name;
+        $scope.start_date = result.data[0].start_date;
         $scope.authFlag = result.data[0].authFlag;
         $scope.infoFlag = result.data[0].infoFlag;
-        console.log($scope.infoFlag);
+        $scope.legal_person = result.data[0].legal_person;
+        $scope.credit = result.data[0].credit;
+        $scope.asset = result.data[0].asset;
+        $scope.userList = result.data;
+        $scope.isPending = false;
+        console.log(result.data[0]);
     });
+
+    var register = function() {
+        $scope.isPending = true;
+        $state.go('app.account');
+    }
 
 };
